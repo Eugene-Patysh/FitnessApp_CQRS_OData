e76320afe7e1ea16ef9953e76413452a419f5b75
Eugene Patysh <eugene.patysh@gmail.com>
2023-07-17 18:45:35 +0300

    82

--- FitnessApp_CQRS_OData.ClientApp/src/environments/set-env.ts
+++ FitnessApp_CQRS_OData.ClientApp/src/environments/set-env.ts
@@ -4,8 +4,6 @@ const setEnv = () => {
   const writeFile = fs.writeFile;
 // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
-// Load node modules
-  const colors = require('colors');
   // const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env'
@@ -16,14 +14,14 @@ const setEnv = () => {
     apiUrl: '${process.env["API_URL"]}',
   };
 `;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-  console.log(colors.grey(envConfigFile));
+  console.log('The file `environment.ts` will be written with the following content: \n');
+  console.log(envConfigFile);
   writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
     }
   });
 };
